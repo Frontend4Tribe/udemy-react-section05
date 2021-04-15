d0e64c98005d5ee44406e42ebe39e3597f4032dc
RearrangeWorld <80311827+RearrangeWorld@users.noreply.github.com>
2021-04-15 13:28:52 +0900

    ルーティングの基礎（React Router）

---
+++ src/Home.jsx
@@ -0,0 +1,7 @@
+export const Home = () => {
+  return (
+    <div>
+      <h1>This page is Home.</h1>
+    </div>
+  );
+};
---
+++ src/Page1DetailB.jsx
@@ -0,0 +1,7 @@
+export const Page1DetailB = () => {
+  return (
+    <div>
+      <h1>This page is Page1DetailB.</h1>
+    </div>
+  );
+};
